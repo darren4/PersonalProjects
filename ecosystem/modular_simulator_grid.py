dd7abd22cd0ca1c7e7a7f3a06ab80598f704d293
tascillo <tascillo@umich.edu>
2023-10-04 08:01:12 -0500

    progress

--- ecosystem/modular_simulator_grid.py
+++ ecosystem/modular_simulator_grid.py
@@ -6,10 +6,15 @@ random.seed(728)
 
 
 grid = None
-grid_shape = None
+grid_shape = None # todo: make reader lock
 
 day = -1
 day_lock = Lock()
+day_cv = Condition(day_lock)
+
+ready = 0
+ready_lock = Lock()
+ready_cv = Condition(ready_lock)
 
 def new_position(ri, ci, grid_shape):
   valid_positions = []
@@ -43,7 +48,58 @@ def new_position(ri, ci, grid_shape):
 
 def process_position(ri, ci):
   current_day = 0
-  
+  while True:
+    with day_lock:
+      day_cv.wait_for(current_day == day)
+
+    with grid[ri][ci][0]["POSITION_LOCK"]:
+      surviving_prey = []
+      for _ in range(len(grid[ri][ci][0]["PREY"])):
+        current_prey = grid[ri][ci][0]["PREY"].pop()
+        survived = True
+
+        if grid[ri][ci][0]["STRONG_PREDATOR"] and current_prey.inherited["STRENGTH"] < grid[ri][ci][0]["PREDATOR_TOP_STRENGTH"]:
+          print("Prey eaten")
+          survived = False
+        elif current_prey.starved(grid[ri][ci][0]["FOOD"]):
+          print("Prey starved")
+          survived = False
+
+        if survived:
+          surviving_prey.append(current_prey)
+      
+      next_prey = []
+      next_prey.extend(surviving_prey)
+      if surviving_prey:
+        random.shuffle(surviving_prey)
+        correction = 0
+        if len(surviving_prey) % 2 == 1:
+          correction = 1
+        for left_idx in range(0, len(surviving_prey) - correction, 2):
+          left_prey, right_prey = surviving_prey[left_idx], surviving_prey[left_idx + 1]
+          new_prey = left_prey.reproduce(right_prey)
+          next_prey.extend(new_prey)
+
+      for prey in next_prey:
+        new_ri, new_ci = new_position(ri, ci, grid_shape)
+        with grid[new_ri][new_ci][1]["POSITION_LOCK"]:
+          grid[new_ri][new_ci][1]["PREY"].append(prey)
+
+      # Process moving predators
+      for predator in grid[ri][ci][0]["PREDATORS"]:
+        new_ri, new_ci = self.new_position(ri, ci)
+        if next_grid[new_p0][new_p1]["STRONG_PREDATOR"]:
+          if predator.strength > next_grid[new_p0][new_p1]["STRONG_PREDATOR"].strength:
+            next_grid[new_p0][new_p1]["WEAK_PREDATORS"].append(next_grid[new_p0][new_p1]["STRONG_PREDATOR"])
+            next_grid[new_p0][new_p1]["STRONG_PREDATOR"] = predator
+          else:
+            next_grid[new_p0][new_p1]["WEAK_PREDATORS"].append(predator)
+        else:
+          next_grid[new_p0][new_p1]["STRONG_PREDATOR"] = predator
+
+    with ready_lock:
+      ready += 1
+      ready_cv.notify_all()
 
 class Organism:
   alive_count = 0
@@ -113,6 +169,7 @@ def run_simulation():
     ri, ci = random.randint(0, grid_shape[0] - 1), random.randint(0, grid_shape[1] - 1)
     with grid[ri][ci][0]["POSITION_LOCK"]:
       grid[ri][ci][0]["FOOD"] += 1
+      grid[ri][ci][1]["FOOD"] += 1
 
   for _ in range(start_predator_count):
     ri, ci = random.randint(0, grid_shape[0] - 1), random.randint(0, grid_shape[1] - 1)
@@ -131,15 +188,22 @@ def run_simulation():
     for ci in range(grid_shape[1]):
       Thread(target=process_position, args=[ri, ci]).start()
 
+  worker_count = grid_shape[0] * grid_shape[1]
+
   for i in range(day_count):
     with day_lock:
       print(f"--- Day {day} ---")
       day = i
+      day_cv.notify_all()
+
+    with ready_lock:
+      ready_cv.wait_for(ready == worker_count)
 
     with Organism.alive_count_lock:
       if Organism.alive_count == 0:
         break
       
+    # TODO: make workers do this (put at end of function)
     for ri in range(grid_shape[0]):
       for ci in range(grid_shape[1]):
         with grid[ri][ci][0]["POSITION_LOCK"]:
