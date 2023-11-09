3e5f6aaa79eeb04dd3e1d196ba4db0b09ffe4da9
tascillo <tascillo@umich.edu>
2023-11-08 19:49:53 -0600

    cleaning

--- ecosystem/cplusplus/random_numbers.cpp
+++ ecosystem/cplusplus/random_numbers.cpp
@@ -2,12 +2,6 @@
 #include <cstdlib>
 #include <assert.h> 
 
-/*
-float random_float(float start, float end){
-    return start + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(end-start)));
-}
-*/
-
 
 size_t random_int(size_t start, size_t end){
     assert(start <= end);
--- ecosystem/cplusplus/random_numbers.h
+++ ecosystem/cplusplus/random_numbers.h
@@ -3,8 +3,6 @@
 #include <cstdlib>
 
 
-// float random_float(float start, float end);
-
 size_t random_int(size_t start, size_t end);
 
 
