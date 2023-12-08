f0c7249814490fd04d96a026ef57893172457953
tascillo <tascillo@umich.edu>
2023-12-07 21:51:12 -0600

    finished initial exchange

--- exchange/exchange.py
+++ exchange/exchange.py
@@ -11,35 +11,64 @@ AMOUNT = "AMOUNT"
 ORDER = "ORDER"
 MATCH = "MATCH"
 
-class Exchange:
+CASH = "CASH"
+SECURITY = "SECURITY"
 
-    def __init__(self):
+
+class Exchange:
+    def __init__(self, participants):
+        """
+        participants: dictionary of participant_id (key) to dictionary of cash/security amount (value)
+        """
         self._buy_order_queue = PriorityQueue()
         self._sell_order_queue = PriorityQueue()
         self.tape = []
 
-    def trade(self, participant_id, action, price: float, amount: int):
+        self._participants = participants
+
+    def trade(self, participant_id: int, action: str, price: float, amount: int):
         order = {
             PARTICIPANT_ID: participant_id,
             ACTION: action,
             PRICE: price,
-            AMOUNT: amount
+            AMOUNT: amount,
         }
         if action == BUY:
+            if order[AMOUNT] * order[PRICE] > self._participants[participant_id][CASH]:
+                raise ValueError("insufficient funds")
             self._buy_order_queue.put((-price, order))
         elif action == SELL:
+            if order[AMOUNT] > self._participants[participant_id][SECURITY]:
+                raise ValueError("insufficient security holdings")
             self._sell_order_queue.put((price, order))
         else:
             raise ValueError("action must be BUY or SELL")
         self.tape.append((ORDER, order))
 
-    def _try_match_trade(self) -> bool:
+    def try_match_trade(self) -> bool:
         try:
-            largest_buy = self._buy_order_queue.get()[1]
-            largest_sell = self._sell_order_queue.get()[1]
+            generous_buy_queue_elt = self._buy_order_queue.get()
+            generous_sell_queue_elt = self._sell_order_queue.get()
+            generous_buy = generous_buy_queue_elt[1]
+            generous_sell = generous_sell_queue_elt[1]
         except Empty:
             return False
-        
-        
+        if generous_buy[PRICE] < generous_sell[PRICE]:
+            return False
 
+        if generous_buy[AMOUNT] >= generous_sell[AMOUNT]:
+            generous_buy_queue_elt[1][AMOUNT] = (
+                generous_buy[AMOUNT] - generous_sell[AMOUNT]
+            )
+            self._buy_order_queue.put(generous_buy_queue_elt)
+        else:
+            generous_sell_queue_elt[1][AMOUNT] = (
+                generous_sell[AMOUNT] - generous_buy[AMOUNT]
+            )
+            self._sell_order_queue.put(generous_sell_queue_elt)
+        self.tape.append((MATCH, generous_buy, generous_sell))
+        return True
+
+
+class Participant:
     
