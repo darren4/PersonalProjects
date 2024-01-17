cf887322a1527a4d902745ac3743bbb46c13e807
tascillo <tascillo@umich.edu>
2024-01-17 07:00:42 -0600

    adding hugging face vectorizer

---
+++ nlp/vectorize/vectorize_with_hg.py
@@ -0,0 +1,13 @@
+from vectorize.base_vectorize import BaseVectorize
+
+from typing import List
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
+
+class VectorizeWithHG(BaseVectorize):
+    def __init__(self, model_name="all-MiniLM-L6-v2"):
+        self._model = SentenceTransformer(model_name)
+
+    def vectorize(self, strings: List[str]) -> List[np.array]:
+        return self._model.encode(strings, convert_to_numpy=True)
