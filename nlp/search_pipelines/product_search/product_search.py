0080c00ac3445447a5c6d3242a4a3d38b243d9a8
tascillo <tascillo@umich.edu>
2024-03-24 17:32:32 -0500

    adjusted dedictaed glove search

--- nlp/search_pipelines/product_search/product_search.py
+++ nlp/search_pipelines/product_search/product_search.py
@@ -1,4 +1,5 @@
 # %%
+from nlp.glove.train import train_glove_vectors
 from nlp.vectorize.base_vectorize import BaseVectorize
 from nlp.vectorize.vectorize_with_hg import VectorizeWithHG
 from nlp.vectorize.vectorize_with_dict import VectorizeWithDict
@@ -12,28 +13,40 @@ import os
 import time
 import json
 import pandas as pd
-from typing import Final, Dict, List
+from typing import Final, Dict, List, Tuple
 
 BASE_PATH: Final[str] = f"{os.getenv('PYTHONPATH')}/nlp/search_pipelines/product_search"
 
 
 logger = Logger(f"{BASE_PATH}/logs.txt")
 
-def jsonable_to_str(json_obj):
-    return json.dumps(json_obj, separators=(",", ":"))
+
+def describe_product(record: dict):
+    name: str = record["product_name"]
+    manufacturer: str = record["manufacturer"]
+    return f"Product {name} built by {manufacturer}"
 
 
-print(f"{BASE_PATH}/amazon_products.csv")
+def process_product(record: dict):
+    return clean_str(describe_product(record))
+
+
+# %%
 raw_data = pd.read_csv(f"{BASE_PATH}/amazon_products.csv")
-print(f"Product data length: {len(raw_data)}")
-raw_data = raw_data.head(1000)
+logger.log(f"Product data length: {len(raw_data)}")
+start_time = time.time()
 raw_data = raw_data[["product_name", "manufacturer"]]
 json_dicts = raw_data.to_dict("records")
-json_strs = [clean_str(json.dumps(json_dict)) for json_dict in json_dicts]
+json_strs = [process_product(json_dict) for json_dict in json_dicts]
+logger.log(f"Preprocessing took {time.time() - start_time} seconds")
 
 # %%
 start_time = time.time()
-vector_dict: Dict[str, List] = read_vectors_from_path(f"{BASE_PATH}/vectors.txt")
+vector_dict = train_glove_vectors(json_strs)
+print(f"Training embeddings took {time.time() - start_time} seconds")
+
+# %%
+start_time = time.time()
 vectorizer: BaseVectorize = VectorizeWithDict(vector_dict, 10)
 companies_vectors = vectorizer.vectorize(json_strs)
 logger.log(f"Vectorizing {len(companies_vectors)} strings took {time.time() - start_time} seconds")
@@ -47,11 +60,11 @@ logger.log(f"Storing vectors took {time.time() - start_time} seconds")
 
 # %%
 query_dict = {
-    "product_name": "Ohigawa Railway SL Kawane-ji Model Train",
-    "manufacturer": "Kato"
+    "product_name": "Oldtime Lanterns Working",
+    "manufacturer": "Busch"
 }
 start_time = time.time()
-query_str = json.dumps(query_dict)
+query_str = process_product(query_dict)
 query_vector = vectorizer.vectorize([query_str])[0]
 result_ids = search.search(query_vector)
 logger.log(f"Query took {time.time() - start_time} seconds")
