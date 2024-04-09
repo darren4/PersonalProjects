ea733cdcbb7eb2ae2e74601922042d61aaa51e54
tascillo <tascillo@umich.edu>
2024-04-08 21:37:00 -0500

    adjusted default

--- nlp/vectorize/vectorize_with_huggingface_transformer.py
+++ nlp/vectorize/vectorize_with_huggingface_transformer.py
@@ -7,7 +7,7 @@ import torch
 
 
 class VectorizeWithHG(BaseVectorize):
-    def __init__(self, model_name="all-MiniLM-L6-v2"):
+    def __init__(self, model_name="sentence-t5-base"):
         self._model: SentenceTransformer = SentenceTransformer(model_name)
 
     def vectorize(
